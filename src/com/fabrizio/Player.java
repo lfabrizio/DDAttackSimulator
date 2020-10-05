4ddf0c12460bc479a9dcb48a10270355dc3db551
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-10-05 18:51:52 -0400

    update

--- src/com/fabrizio/Player.java
+++ src/com/fabrizio/Player.java
@@ -4,8 +4,41 @@ public class Player {
     private int attacker;
     private int hitPoint;
     private int armour;
+    private int defense;
 
-    public Player(){
+    public Player(int attacker, int hitPoint, int armour, int defense){
 
+    }
+
+    public int getAttacker() {
+        return attacker;
+    }
+
+    public void setAttacker(int attacker) {
+        this.attacker = attacker;
+    }
+
+    public int getHitPoint() {
+        return hitPoint;
+    }
+
+    public void setHitPoint(int hitPoint) {
+        this.hitPoint = hitPoint;
+    }
+
+    public int getArmour() {
+        return armour;
+    }
+
+    public void setArmour(int armour) {
+        this.armour = armour;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
     }
 }
