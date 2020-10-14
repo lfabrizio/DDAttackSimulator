2ee818dee15b07adf8bb56780e0dd8adb30b315c
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-10-14 17:31:14 -0400

    user console addition

--- src/com/fabrizio/Console.java
+++ src/com/fabrizio/Console.java
@@ -1,5 +1,18 @@
 package com.fabrizio;
 
-public class Console {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Console {
+    public static List<Integer> userChoices() {
+
+        List<Integer> modifiers = new ArrayList<Integer>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("armor: ");
+        modifiers.add(scanner.nextInt());
+        System.out.print("Defense: ");
+        modifiers.add(scanner.nextInt());
+        return modifiers;
+    }
 }
